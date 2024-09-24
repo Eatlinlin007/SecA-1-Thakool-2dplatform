381d97dffa1a0cfbbcdcc2fd0c35db539e503515
Eatlinlin007 <thakool.srip@bumail.net>
2024-09-24 11:23:47 +0700

    Character

---
+++ Assets/Lab/Character.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Character : MonoBehaviour
+{
+    private int health;
+    public int Health
+    {
+        get
+        {
+            return health;
+        }
+        set
+        {
+            health = value;
+        }
+    }
+    public Animation anim;
+    public Rigidbody2D rb;
+
+    public bool IsDead()
+    {
+        return Health <= 0;
+    }
+    public void OnConnectedToServer(int damage)
+    {
+        Health -= damage;
+    }
+}
